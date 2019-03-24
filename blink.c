f52fc8a4585f2a1521d27e60b9c4bb0447688b94
Your Name <you@example.com>
2019-03-24 19:47:19 +0400

    ATmega32u2 config + DFU

--- blink.c
+++ blink.c
@@ -1,4 +1,4 @@
-#define F_CPU 1000000UL
+#define F_CPU 2000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -6,16 +6,46 @@
 int main ()
 {
 	DDRB |= (1 << PB0);
+	DDRB |= (0 << PB4);
+	PORTB |= (1 << PB0);
+	PORTB |= (1 << PB4);
+	
 
 	while(1) 
 	{
-		PORTB ^= (1 << PB0);
+		
+		//_delay_ms(1000);
+		//_delay_ms(1000);
+		//_delay_ms(1000);
+		//_delay_ms(1000);
+		//_delay_ms(1000);
+		//_delay_ms(1000);
 		_delay_ms(1000);
-		_delay_ms(1000);
-		_delay_ms(1000);
-		_delay_ms(1000);
-		_delay_ms(1000);
-		_delay_ms(1000);
-		_delay_ms(1000);
-	}
+		
+		int a = read_bit_from_B(PB4);
+		set_bit_by_B(PB0, a);
+	}	
+	
 }
+
+int read_bit_from_B(int leg)
+{
+	 
+	 int a;
+	 a = PINB >> leg;
+	 return a & 1;
+ }
+ 
+
+ 
+int set_bit_by_B(int leg, int bit){
+	 int b = 1<<leg;
+	 
+	 if(bit==1){
+		  PORTB = PORTB | b ;
+	  }
+	  else {
+		  PORTB = PORTB	& (255-b);
+	  }
+	 
+}	 
--- dfu.sh
+++ dfu.sh
@@ -1,4 +1,7 @@
 dfu-programmer atmega32u2 reset
+sleep 1
 dfu-programmer atmega32u2 erase
+sleep 1
 dfu-programmer atmega32u2 flash blink.hex
+sleep 1
 dfu-programmer atmega32u2 reset
